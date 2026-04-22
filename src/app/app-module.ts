c38999cd9c0a0b5a05c02a5c624253610f68b13c
Suriel Francisco <surielf39@gmail.com>
2026-04-22 09:10:57 -0600

    Agregue los componentes necesarios

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './header/header';
+import { WorkExperience } from './work-experience/work-experience';
+import { Education } from './education/education';
+import { Skills } from './skills/skills';
+import { Certificates } from './certificates/certificates';
+import { Languages } from './languages/languages';
+import { Interests } from './interests/interests';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    WorkExperience,
+    Education,
+    Skills,
+    Certificates,
+    Languages,
+    Interests
   ],
   imports: [
     BrowserModule,
